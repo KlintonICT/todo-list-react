2e9ece9c4273ea1d0405321cfa608ddf5114c3c9
Klinton Chhun <kt@c.local>
2023-06-16 09:41:54 +0700

    set up antd and tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
-function App() {
-  return <div>Todo List App</div>;
-}
+const App = () => {
+  return (
+    <div className="w-full text-center p-10">
+      <div className="max-w-full md:max-w-[50%] lg:max-w-[45%] w-full mx-auto mt-4">
+        <h1 className="text-3xl font-bold">Todo App</h1>
+      </div>
+    </div>
+  );
+};
 
 export default App;
